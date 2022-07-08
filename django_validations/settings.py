fcf06952842edeb22e01db9b76a9ee2253228a11
Nathan D Leathers <nathan.d.leathers@icloud.com>
2022-07-07 20:25:18 -0700

    wrote validators, cant run tests

--- django_validations/settings.py
+++ django_validations/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-TEST_RUNNER="redgreenunittest.django.runner.RedGreenDiscoverRunner"
+TEST_RUNNER = "redgreenunittest.django.runner.RedGreenDiscoverRunner"
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- swimrecords/models.py
+++ swimrecords/models.py
@@ -1,12 +1,30 @@
+from ssl import Options
 from django.db import models
+from django.core.validators import *
+from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator
+from datetime import date
+from django.utils.translation import gettext_lazy as text  # import gettext_lazy
+
+
+def validate_stroke(stroke):
+    STROKE_CHOICES = ['front crawl', 'butterfly',
+                      'breast', 'back', 'freestyle']
+    if stroke not in STROKE_CHOICES:
+        raise ValidationError(
+            text(f"{stroke} is not valid. Please select a valid stroke type."))
+
 
 class SwimRecord(models.Model):
-    pass # delete me when you start writing in validations
-    # first_name = models.CharField()
-    # last_name = models.CharField()
-    # team_name = models.CharField()
-    # relay = models.BooleanField()
-    # stroke = models.CharField()
-    # distance = models.IntegerField()
-    # record_date = models.DateTimeField()
-    # record_broken_date = models.DateTimeField()
+
+    first_name = models.CharField(max_length=255, null=False)
+    last_name = models.CharField(max_length=255, null=False, default=None)
+    team_name = models.CharField(max_length=255, null=False, default=None)
+    relay = models.BooleanField(default=True)
+    stroke = models.CharField(max_length=255, validators=[
+                              validate_stroke], null=False, default=None)
+    distance = models.IntegerField(
+        validators=[MinValueValidator(50)], null=False, default=None)
+    record_date = models.DateTimeField(
+        validators=[MaxValueValidator(limit_value=date.today)], default=None)
+    record_broken_date = models.DateTimeField(validators=[MaxValueValidator(limit_value=record_date)], default=None)
